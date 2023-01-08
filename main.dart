29f95401608e5456bf8a42119dcef019de3dc9b0
Junsu <pksjmh5295@gmail.com>
2023-01-08 23:34:18 +0900

    1.2 Dynamic Type

--- main.dart
+++ main.dart
@@ -1,8 +1,20 @@
 void main() {
-  // You can update variable.
-  var name = 'Junsu'; // Dart style
+  // Not recommended, but useful... may be?
+  dynamic name = "Junsu";
+
+  // You can define like this
+  // var name;
+
+  name = 1;
+  name = false;
   name = "Unknown";
 
-  // String name = "JS";
-  // name = "Unknown";
+  if (name is String) {
+    name.toLowerCase();
+  } else if (name is int) {
+    name.isInfinite;
+    name.abs();
+  } else {
+    return;
+  }
 }
