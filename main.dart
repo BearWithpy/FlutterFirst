9a2ad37881c47f8a2d033b7b14d555fc8b69d743
Junsu <pksjmh5295@gmail.com>
2023-01-08 23:49:07 +0900

    1.4 Final Variables

--- main.dart
+++ main.dart
@@ -1,11 +1,4 @@
 void main() {
-  // Null safety: 개발자가 Null 값을 참조하지 않도록 하는 것, 런타임 에러 방지
-
-  String? name = "jason";
-  name = null;
-
-  // if (name != null) {
-  //   name.isNotEmpty;
-  // }
-  name?.isNotEmpty;
+  // const;
+  final nameJs = "Junsu";
 }
