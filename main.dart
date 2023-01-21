44fd6c604d4c87dcad4fa049709da0525d5f54bd
Junsu <pksjmh5295@gmail.com>
2023-01-21 21:29:02 +0900

    2.4 Maps

--- main.dart
+++ main.dart
@@ -1,37 +1,28 @@
 void main() {
-  var giveMeSix = true;
+  var wow = {
+    1: true,
+    2: false,
+    3: "false",
+  };
 
-  // List<int>
-  var numbers = [
-    1,
-    2,
-    3,
-    4,
-    if (giveMeSix) 6,
+  Map<int, bool> player = {
+    1: true,
+    2: false,
+    3: false,
+  };
+
+  Map<List<int>, bool> hmm = {
+    [1, 2, 3, 4]: true,
+  };
+
+  List<Map<String, Object>> people = [
+    {
+      "name": "jason",
+      "email": "pks5294@naver.com",
+    },
+    {
+      "name": "jj",
+      "phone": 010101299874108289,
+    },
   ];
-  // numbers.add(5);
-
-  print(numbers);
-
-  // #2.2 String Interpolation
-  var name = "jason";
-  var age = 22;
-  var greeting =
-      'Hello, Everyone!!\nMy name is $name.\nNice to Meet you~~\nI\'m ${age + 2} years old';
-
-  print(greeting);
-
-  // #2.3 collection for
-  var oldFriend = [
-    'junsu',
-    'suyeon',
-  ];
-  var newFriend = [
-    'lewis',
-    'ralph',
-    'sue',
-    for (var person in oldFriend) "🥰 $person",
-  ];
-
-  print(newFriend);
 }
