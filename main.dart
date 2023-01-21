1429ff92abaf05a0de6ea6f1b05171c7f593b959
Junsu <pksjmh5295@gmail.com>
2023-01-21 21:31:47 +0900

    2.5 Sets

--- main.dart
+++ main.dart
@@ -1,28 +1,5 @@
 void main() {
-  var wow = {
-    1: true,
-    2: false,
-    3: "false",
-  };
-
-  Map<int, bool> player = {
-    1: true,
-    2: false,
-    3: false,
-  };
-
-  Map<List<int>, bool> hmm = {
-    [1, 2, 3, 4]: true,
-  };
-
-  List<Map<String, Object>> people = [
-    {
-      "name": "jason",
-      "email": "pks5294@naver.com",
-    },
-    {
-      "name": "jj",
-      "phone": 010101299874108289,
-    },
-  ];
+  // Set - Curly Bracket;
+  Set<int> st = {1, 2, 3};
+  var s = {1, 2, 3, 4, 5};
 }
